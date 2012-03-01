94c75695d8922654d9c99b19ec5087beebca3b33
michal.malohlava <michal.malohlava@5bbc420b-fa36-85c3-ac9f-c109973bdd2e>
2012-03-01 16:07:41 +0000

    minor changes

--- trunk/2011-12/01-osgi/demo03/logger-bundle/src/cz/cuni/mff/dsrg/log/impl/LoggerImpl.java
+++ trunk/2011-12/01-osgi/demo03/logger-bundle/src/cz/cuni/mff/dsrg/log/impl/LoggerImpl.java
@@ -6,21 +6,31 @@ package cz.cuni.mff.dsrg.log.impl;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.osgi.service.component.ComponentContext;
+
 import cz.cuni.mff.dsrg.log.api.ILogTarget;
 import cz.cuni.mff.dsrg.log.api.ILogger;
 
 /**
+ * 
  * @author Michal Malohlava
- *
  */
 public class LoggerImpl implements ILogger {
 	
-	List<ILogTarget> logTargets = new LinkedList<ILogTarget>();
+	private String prefix = null; 
+	private List<ILogTarget> logTargets = new LinkedList<ILogTarget>();
+	
+	protected void activate(ComponentContext ctx) {
+		System.out.println("LoggerImpl.activate()");
+		
+		prefix = (String) ctx.getProperties().get("logPrefix");
+		prefix = prefix != null ? prefix : "";
+	}
 
 	@Override
 	public void info(String message) {
 		for (ILogTarget target : logTargets) {
-			target.write("[" + target.getName() +  "]: " + message);
+			target.write(prefix + "[" + target.getName() +  "]: " + message);
 		}		
 	}
 	
