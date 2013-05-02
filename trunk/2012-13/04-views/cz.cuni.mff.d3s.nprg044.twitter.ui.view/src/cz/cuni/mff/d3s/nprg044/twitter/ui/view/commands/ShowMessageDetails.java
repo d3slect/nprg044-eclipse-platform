a693a2e551c3e688f9c2a90f8cf67332ae310a1a
pparizek81@gmail.com <pparizek81@gmail.com@5bbc420b-fa36-85c3-ac9f-c109973bdd2e>
2013-05-02 08:26:27 +0000

    lecture 4

---
+++ trunk/2012-13/04-views/cz.cuni.mff.d3s.nprg044.twitter.ui.view/src/cz/cuni/mff/d3s/nprg044/twitter/ui/view/commands/ShowMessageDetails.java
@@ -0,0 +1,23 @@
+package cz.cuni.mff.d3s.nprg044.twitter.ui.view.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class ShowMessageDetails extends AbstractHandler {
+
+	// response for a particular selection in the context menu
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		try {
+			// show the properties dialog
+			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView("org.eclipse.ui.views.PropertySheet");
+		} catch (PartInitException e) {
+			throw new ExecutionException(e.getMessage(), e);
+		}	
+		return null;
+	}
+
+}
