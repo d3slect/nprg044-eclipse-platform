0b8f07b24ca900a8d4fe6cb1404c92450466ef14
pparizek81@gmail.com <pparizek81@gmail.com@5bbc420b-fa36-85c3-ac9f-c109973bdd2e>
2014-02-24 13:33:56 +0000

    lecture about OSGi

---
+++ trunk/2013-14/01-osgi/demo02/demo02-impl-decl/src/cz/cuni/mff/d3s/nprg044/tut1/demo02/impl_decl/ServiceComponentImpl.java
@@ -0,0 +1,46 @@
+package cz.cuni.mff.d3s.nprg044.tut1.demo02.impl_decl;
+
+import java.util.Dictionary;
+import java.util.Properties;
+
+import org.osgi.service.component.ComponentFactory;
+import org.osgi.service.component.ComponentInstance;
+
+import cz.cuni.mff.d3s.nprg044.tut1.demo02.api.IHelloService;
+import cz.cuni.mff.d3s.nprg044.tut1.demo02.api.IService;
+
+public class ServiceComponentImpl implements IService {
+
+	private IHelloService helloService;
+
+	public void test() {
+		System.out.println("Test service!");
+	}
+	
+	public void activate() {
+		System.out.println("Service component activated!");		
+	}
+	
+	public void bindHelloService(IHelloService helloService) {
+		System.out.println("Binding hello service to: " + helloService);
+		
+		this.helloService = helloService;
+		
+		// use hello service
+		this.helloService.greet();
+	}
+	
+	public void bindCF(ComponentFactory cf) {
+		System.out.println("Binding component factory to: " + cf);
+		
+		Dictionary dict = new Properties() {{ put("greeting", "hi"); }};
+		
+		// get component instance
+		System.out.println("Creating new instance of the component");
+		ComponentInstance ci = cf.newInstance(dict);
+	}
+	
+	public void unbindCF(ComponentFactory cf) {		
+	}
+
+}
---
+++ trunk/2013-14/01-osgi/demo02/demo02-impl-prog/src/cz/cuni/mff/d3s/nprg044/tut1/demo02/impl_prog/ServiceImpl.java
@@ -0,0 +1,10 @@
+package cz.cuni.mff.d3s.nprg044.tut1.demo02.impl_prog;
+
+import cz.cuni.mff.d3s.nprg044.tut1.demo02.api.IService;
+
+public class ServiceImpl implements IService {
+	public void test() {
+		System.out.println("Test!");
+	}
+
+}
