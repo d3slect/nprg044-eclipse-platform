941257325231ece736387e39a244ccdb2f72102f
pparizek81@gmail.com <pparizek81@gmail.com@5bbc420b-fa36-85c3-ac9f-c109973bdd2e>
2014-05-05 12:57:37 +0000

    lecture 04

---
+++ 2013-14/04-adv-ui-wizards/cz.cuni.mff.d3s.nprg044.twitter.ui.preferences/src/cz/cuni/mff/d3s/nprg044/twitter/ui/preferences/TwitterPreferencesConstants.java
@@ -0,0 +1,11 @@
+package cz.cuni.mff.d3s.nprg044.twitter.ui.preferences;
+
+public interface TwitterPreferencesConstants {
+	
+	public static final String OATH_CONSUMER_KEY = "t4eclipse.oath.consumer.key";	
+	public static final String OATH_CONSUMER_SECRET = "t4eclipse.oath.consumer.secret";
+	
+	public static final String USERNAME = "t4eclipse.username";
+	public static final String PASSWORD = "t4eclipse.password";
+	
+}
