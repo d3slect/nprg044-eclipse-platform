c716d238ecd5d08b58714486736b514ff6aba077
jiri_vinarek <jiri.vinarek@gmail.com>
2016-03-21 16:22:00 +0100

    added @Override annotations

--- 01-osgi/demo03/simple-log-target/src/cz/cuni/mff/d3s/nprg044/log/impl/SimpleLogTargetImpl.java
+++ 01-osgi/demo03/simple-log-target/src/cz/cuni/mff/d3s/nprg044/log/impl/SimpleLogTargetImpl.java
@@ -4,10 +4,12 @@ import cz.cuni.mff.d3s.nprg044.log.api.ILogTarget;
 
 public class SimpleLogTargetImpl implements ILogTarget {
 
+	@Override
 	public void write(String message) {
 		System.out.println(message);
 	}
 
+	@Override
 	public String getName() {
 		return "System out";
 	}
