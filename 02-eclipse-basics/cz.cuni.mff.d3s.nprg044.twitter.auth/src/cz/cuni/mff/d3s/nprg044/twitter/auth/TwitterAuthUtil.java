19a4d8d0542f1ad5cb533de6e319ca7b9de2eac5
Jiri Vinarek <vinarek@d3s.mff.cuni.cz>
2016-04-14 21:21:57 +0200

    Credentials loadeding from .properties file

--- 02-eclipse-basics/cz.cuni.mff.d3s.nprg044.twitter.auth/src/cz/cuni/mff/d3s/nprg044/twitter/auth/TwitterAuthUtil.java
+++ 02-eclipse-basics/cz.cuni.mff.d3s.nprg044.twitter.auth/src/cz/cuni/mff/d3s/nprg044/twitter/auth/TwitterAuthUtil.java
@@ -1,26 +1,35 @@
 package cz.cuni.mff.d3s.nprg044.twitter.auth;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-
 public class TwitterAuthUtil {
 
-	public static Twitter getTwitterInstance()
-	{
+	public static Twitter getTwitterInstance() {
 		ConfigurationBuilder cb = new ConfigurationBuilder();
-		
-		// authentication credentials
-		cb.setOAuthConsumerKey("******");
-		cb.setOAuthConsumerSecret("******");
-		cb.setOAuthAccessToken("******");
-		cb.setOAuthAccessTokenSecret("******");
-	
+
+		try (InputStream in = TwitterAuthUtil.class.getResourceAsStream("/twitter4j.properties")) {
+			Properties properties = new Properties();
+			properties.load(in);
+
+			// authentication credentials
+			cb.setOAuthConsumerKey(properties.getProperty("oauth.consumerKey"));
+			cb.setOAuthConsumerSecret(properties.getProperty("oauth.consumerSecret"));
+			cb.setOAuthAccessToken(properties.getProperty("oauth.accessToken"));
+			cb.setOAuthAccessTokenSecret(properties.getProperty("oauth.accessTokenSecret"));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		TwitterFactory tf = new TwitterFactory(cb.build());
-		
 		Twitter twitter = tf.getInstance();
-		
+
 		return twitter;
 	}
 }
