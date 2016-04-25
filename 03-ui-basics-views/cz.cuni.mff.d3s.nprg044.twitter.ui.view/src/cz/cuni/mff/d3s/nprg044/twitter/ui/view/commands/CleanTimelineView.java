d20b76dc089b2d25dae9ec125281eb91f447d978
Jiri Vinarek <vinarek@d3s.mff.cuni.cz>
2016-04-25 20:16:06 +0200

    added @CanExecute to enable handler only in TwitterMessageTimelineView

--- 03-ui-basics-views/cz.cuni.mff.d3s.nprg044.twitter.ui.view/src/cz/cuni/mff/d3s/nprg044/twitter/ui/view/commands/CleanTimelineView.java
+++ 03-ui-basics-views/cz.cuni.mff.d3s.nprg044.twitter.ui.view/src/cz/cuni/mff/d3s/nprg044/twitter/ui/view/commands/CleanTimelineView.java
@@ -2,6 +2,7 @@ package cz.cuni.mff.d3s.nprg044.twitter.ui.view.commands;
 
 import javax.inject.Named;
 
+import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
@@ -10,6 +11,11 @@ import cz.cuni.mff.d3s.nprg044.twitter.ui.view.TwitterMessageTimelineView;
 
 public class CleanTimelineView {
 
+	@CanExecute
+    public boolean canExecute(@Named(IServiceConstants.ACTIVE_PART) MPart activePart) {
+		return activePart.getObject() instanceof TwitterMessageTimelineView;
+    }	
+	
 	@Execute
 	public void execute(@Named(IServiceConstants.ACTIVE_PART) MPart activePart) {
 		((TwitterMessageTimelineView) activePart.getObject()).cleanTimeline();
