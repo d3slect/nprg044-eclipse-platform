99e72a009ca2be6c75546b48db44af4f5a1cdc38
Jiri Vinarek <vinarek@d3s.mff.cuni.cz>
2016-05-04 11:04:35 +0200

    fixed npe

--- 04-adv-ui-wizards/cz.cuni.mff.d3s.nprg044.twitter.wizards/src/cz/cuni/mff/d3s/nprg044/twitter/wizards/MessageWizardPage.java
+++ 04-adv-ui-wizards/cz.cuni.mff.d3s.nprg044.twitter.wizards/src/cz/cuni/mff/d3s/nprg044/twitter/wizards/MessageWizardPage.java
@@ -12,77 +12,77 @@ import org.eclipse.swt.widgets.Label;
 
 // one page shown in the wizard
 public class MessageWizardPage extends WizardPage {
-	
-	public static final int MAX_CHAR = 150;
-	
-	private TextViewer messageEditor;
-	private Label characterCount;
 
-	protected MessageWizardPage(String pageName) {
-		super(pageName);
-		
-		setTitle("Post a new message");
-		
-		setDescription("Post twitter message. It should not contain more than 150 characters!");
-		
-		setPageComplete(false);
-	}
+    public static final int MAX_CHAR = 150;
 
-	// create layout of the wizard page
-	@Override
-	public void createControl(Composite parent) {
-		final Composite composite = new Composite(parent, SWT.NONE);
-		GridLayout layout = new GridLayout(1, false);
-		composite.setLayout(layout);
-		setControl(composite);
-		
-		messageEditor = new TextViewer(composite, SWT.BORDER | SWT.MULTI);
-		messageEditor.getControl().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));		
-		messageEditor.getTextWidget().addModifyListener(new ModifyListener() {
-			// check the number of characters after each modification
-			@Override
-			public void modifyText(ModifyEvent e) {
-				updateCharactersCount(messageEditor.getTextWidget().getText().length());				
-			}
-		});
-		
-		characterCount = new Label(composite, SWT.NONE);
-		characterCount.setText(MAX_CHAR + " characters");				
-		characterCount.setLayoutData(new GridData(SWT.RIGHT, SWT.BEGINNING, false, false));		
-	}
+    private TextViewer messageEditor;
+    private Label characterCount;
 
-	protected void updateCharactersCount(int length) {		
-		characterCount.setText((MAX_CHAR-length) + " characters");
-		
-		if (validatePage()) {
-			setPageComplete(true);				
-		} 
-		else {
-			setPageComplete(false);
-		}
-	}
-	
-	// validates the user input
-	protected boolean validatePage() {
-		int length = messageEditor.getTextWidget().getText() != null ? messageEditor.getTextWidget().getText().length() : 0;
-	
-		if (length > 0 && length <= MAX_CHAR) {
-			setErrorMessage(null);
-			return true;
-		} 
-		else {
-			setErrorMessage("Character count has to be in range (0..150>" );
-			return false;
-		}
-	}
-	
-	// returns true if this page is complete and we can move to the next one
-	@Override
-	public boolean canFlipToNextPage() {		
-		return validatePage();
-	}
+    protected MessageWizardPage(String pageName) {
+        super(pageName);
 
-	public String getMessageText() {		
-		return messageEditor.getDocument().get();
-	}
+        setTitle("Post a new message");
+
+        setDescription("Post twitter message. It should not contain more than 150 characters!");
+
+        setPageComplete(false);
+    }
+
+    // create layout of the wizard page
+    @Override
+    public void createControl(Composite parent) {
+        final Composite composite = new Composite(parent, SWT.NONE);
+        GridLayout layout = new GridLayout(1, false);
+        composite.setLayout(layout);
+        setControl(composite);
+
+        messageEditor = new TextViewer(composite, SWT.BORDER | SWT.MULTI);
+        messageEditor.getControl().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));     
+        messageEditor.getTextWidget().addModifyListener(new ModifyListener() {
+            // check the number of characters after each modification
+            @Override
+            public void modifyText(ModifyEvent e) {
+                updateCharactersCount(getMessageText().length());                
+            }
+        });
+
+        characterCount = new Label(composite, SWT.NONE);
+        characterCount.setText(MAX_CHAR + " characters");               
+        characterCount.setLayoutData(new GridData(SWT.RIGHT, SWT.BEGINNING, false, false));     
+    }
+
+    protected void updateCharactersCount(int length) {      
+        characterCount.setText((MAX_CHAR-length) + " characters");
+
+        if (validatePage()) {
+            setPageComplete(true);              
+        } 
+        else {
+            setPageComplete(false);
+        }
+    }
+
+    // validates the user input
+    protected boolean validatePage() {
+        int length = getMessageText() != null ? getMessageText().length() : 0;
+
+        if (length > 0 && length <= MAX_CHAR) {
+            setErrorMessage(null);
+            return true;
+        } 
+        else {
+            setErrorMessage("Character count has to be in range (0..150>" );
+            return false;
+        }
+    }
+
+    // returns true if this page is complete and we can move to the next one
+    @Override
+    public boolean canFlipToNextPage() {        
+        return validatePage();
+    }
+
+    public String getMessageText() {        
+        return messageEditor.getTextWidget().getText();
+    }
 }
