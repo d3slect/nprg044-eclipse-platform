d05c9989dd1bc3586635db14966baa6b8ef6ea76
jiri <jiri.vinarek@gmail.com>
2016-05-06 09:30:33 +0200

    updated MarkBoldHandler to use e4 style

--- 05-editor/cz.cuni.mff.d3s.nprg044.twitter.editor/src/cz/cuni/mff/d3s/nprg044/twitter/editor/text/commands/MarkBoldHandler.java
+++ 05-editor/cz.cuni.mff.d3s.nprg044.twitter.editor/src/cz/cuni/mff/d3s/nprg044/twitter/editor/text/commands/MarkBoldHandler.java
@@ -1,41 +1,46 @@
 package cz.cuni.mff.d3s.nprg044.twitter.editor.text.commands;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.TextSelection;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.editors.text.TextEditor;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.internal.e4.compatibility.CompatibilityEditor;
 
-public class MarkBoldHandler extends AbstractHandler {
+@SuppressWarnings("restriction")
+public class MarkBoldHandler {
 
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
-		
-		if (editorPart instanceof TextEditor) {
-			// get current selection in the text editor
-			ISelection selection = HandlerUtil.getCurrentSelection(event);
-			
-			if (selection instanceof TextSelection) {
-				TextEditor editor = (TextEditor) editorPart;
-				IDocument document = editor.getDocumentProvider().getDocument(editor.getEditorInput());				
-				TextSelection textSelection = (TextSelection) selection;
-				try {
-					// put selection into *...* (mark symbol for bold text)
-					document.replace(textSelection.getOffset(), textSelection.getLength(), "*" + textSelection.getText()+ "*");
-				}
-				catch (BadLocationException e) {
-					e.printStackTrace();
+	@Execute
+	public void exec(ESelectionService selectionService, @Named(IServiceConstants.ACTIVE_PART) MPart part) {
+		Object partObject = part.getObject();
+
+		if (partObject instanceof CompatibilityEditor) {
+			CompatibilityEditor compatibilityEditor = (CompatibilityEditor) partObject;
+			if (compatibilityEditor.getEditor() instanceof TextEditor) {
+				TextEditor editor = (TextEditor) compatibilityEditor.getEditor();
+
+				// get current selection in the text editor
+				Object selection = selectionService.getSelection();
+
+				if (selection instanceof TextSelection) {
+					IDocument document = editor.getDocumentProvider().getDocument(editor.getEditorInput());
+					TextSelection textSelection = (TextSelection) selection;
+					try {
+						// put selection into **...** (mark symbol for bold
+						// text)
+						document.replace(textSelection.getOffset(), textSelection.getLength(),
+								"**" + textSelection.getText() + "**");
+					} catch (BadLocationException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		}
-		
-		return null;
 	}
 
 }
